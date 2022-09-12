279a59baaa71b1898af643b410a9a3e84f5c336d
chantoni-jpg <78805182+chantoni-jpg@users.noreply.github.com>
2022-09-12 15:26:52 -0700

    Update model.js

--- app/model.js
+++ app/model.js
@@ -1,4 +1,4 @@
-var key = "65122e7994f94669bc9202250222908";
+var key = "53599ec288054906a0a203018222908";
 var baseURL = "http://api.weatherapi.com/v1/";
 function getCurrentWeather(location) {
   $.get(
